37ce16ea81aa6957f6e7c683b62a963e11481573
limayoung <ayounglim.dev@gmail.com>
2022-05-18 01:41:58 +0900

    20220518/java_programming/wk11/ex1~6

---
+++ java_wk11/src/ex1/PointEqualEx.java
@@ -0,0 +1,24 @@
+package ex1;
+
+public class PointEqualEx {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		Point a = new Point(2,3);
+		Point b = new Point(2,3);
+		Point c = new Point(3,4);
+		
+		if(a == b) 
+			System.out.println("a==b");
+		if(a.equals(b))
+			System.out.println("a is equal to b");
+		if(a.equals(c)) 
+			System.out.println("a is equal to c");
+		
+		*/
+		
+		Rect a = new Rect(2,8);
+		Rect b = new Rect(4,4);
+		Rect c = new Rect(2,7);
+	}
+}
---
+++ java_wk11/src/ex1/Rect.java
@@ -0,0 +1,27 @@
+package ex1;
+
+public class Rect {
+	private int width;
+	private int height;
+	
+	public Rect(int a, int b) {
+		this.width = a; 
+		this.height = b;
+		
+		getArea();
+	}
+	
+	public int getArea() {
+		System.out.println( "∏È¿˚¿∫ "+ width * height );
+		return 0;
+	}
+	
+	public boolean equals(Object obj) {
+		Rect p = (Rect)obj;
+		if(width == p.width && height == p.height) 
+			return true;
+		else
+			return false;
+	}
+
+}
